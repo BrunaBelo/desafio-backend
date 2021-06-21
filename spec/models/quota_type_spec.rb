bee11f379415090674122af2742043993f4cfbb9
BrunaBelo <anabruna28@gmail.com>
2021-06-20 23:04:19 -0300

    Add tests relation between quota_type_specification and quota_type

--- spec/models/quota_type_spec.rb
+++ spec/models/quota_type_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe QuotaType, type: :model do
+  let(:quota_type) { create(:quota_type) }
+
   describe 'associations' do
     describe 'should has one quota type specification' do
-      # pending
+      let!(:quota_type_specification) { create(:quota_type_specification, quota_type: quota_type) }
+     
+      it { expect(quota_type.quota_type_specification).to  be_instance_of(QuotaTypeSpecification) }
     end
 
     describe 'should has many expenses' do
--- spec/models/quota_type_specification_spec.rb
+++ spec/models/quota_type_specification_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe QuotaTypeSpecification, type: :model do
+  let(:quota_type_specification) { create(:quota_type_specification) }
+
   describe 'associations' do
     describe 'should belongs to quota_type' do
-      # pending
+      it { expect(quota_type_specification.quota_type).to be_instance_of(QuotaType) }
     end
   end
 end
