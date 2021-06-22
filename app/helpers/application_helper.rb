010264bdaf7c9cbcd778a4f29befe91c575d99fd
BrunaBelo <anabruna28@gmail.com>
2021-06-21 22:01:09 -0300

    Add message error or success to views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+    def render_message(message, message_type)
+        if message.present?
+            style_type = message_type == 'error' ? 'danger' : 'success'
+            content_tag(:div, content_tag(:span, message), class: "alert alert-#{style_type} mt-1")
+        end
+    end
 end
