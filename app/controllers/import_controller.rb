88f658cade50ed5fd3f90fd3c74f5bb97c039c52
BrunaBelo <anabruna28@gmail.com>
2021-06-21 22:00:19 -0300

    Call service populate database in import controller

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -1,4 +1,9 @@
 class ImportController < ApplicationController
   def import_csv
+    if(Import::PopulateDatabase.populate(params[:file]))
+      redirect_to parliamentarians_path, flash: { success: 'Arquivo importado com sucesso!' }
+    else
+      redirect_to root_path, flash: { error: 'Erro ao importar arquivo csv.' }
+    end
   end
 end
