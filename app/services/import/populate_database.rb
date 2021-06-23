ad6999296d20a6f15218fca4bd5bf7e1dfd883d0
BrunaBelo <anabruna28@gmail.com>
2021-06-22 21:09:39 -0300

    Add function to open file

--- app/services/import/populate_database.rb
+++ app/services/import/populate_database.rb
@@ -19,7 +19,15 @@ module Import
         private
     
         def populate_database_from_csv
-            true
+            error = ''
+            CSV.open(file.path, 'r:bom|UTF-8:utf-8', headers: true, col_sep: ';', skip_blanks: true) do |csv|
+                csv.each do |row|
+                    
+                end
+            rescue CSV::MalformedCSVError
+                error = 'Arquivo CSV mal formatado! Verifique a formatação do arquivo e tente novamente.'
+            end
+            error
         end
     end
 end
