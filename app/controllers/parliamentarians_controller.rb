969362248507c749dd94485af7a3a532fe254f1f
BrunaBelo <anabruna28@gmail.com>
2021-06-23 10:28:55 -0300

    Add layout index parlamentarian view

--- app/controllers/parliamentarians_controller.rb
+++ app/controllers/parliamentarians_controller.rb
@@ -1,6 +1,7 @@
 class ParliamentariansController < ApplicationController
   def index
     @parliamentarians = Parliamentarian.all
+    @parliamentarians = @parliamentarians.paginate(page: params[:page], per_page: 10)
   end
 
   def show
