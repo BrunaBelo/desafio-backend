47cf84f6c6f4306134475dcd65d1d0c5cdb0d565
BrunaBelo <anabruna28@gmail.com>
2021-06-22 23:24:52 -0300

    Add test when import csv file without mandatory field

--- spec/requests/import_spec.rb
+++ spec/requests/import_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe "Imports", type: :request do
   let(:path_to_file) { Rails.root.join 'spec/fixtures' }
   let(:file) { Rack::Test::UploadedFile.new "#{path_to_file}/file.csv", 'text/csv' }
   let(:file_with_error) { Rack::Test::UploadedFile.new "#{path_to_file}/file_with_error.csv", 'text/csv' }
+  let(:file_without_mandatory_field) { Rack::Test::UploadedFile.new "#{path_to_file}/file_without_mandatory_field.csv", 'text/csv' }
 
   describe "POST /import_csv" do
     context 'when the datas is successfully imported' do
@@ -22,6 +23,11 @@ RSpec.describe "Imports", type: :request do
       it 'should return message error' do
         post import_csv_path, params: { file: file_with_error }
         expect(flash[:error]).to eq('Arquivo CSV mal formatado! Verifique a formatação do arquivo e tente novamente.')
+      end
+
+      it 'should return error when without mandatory field' do
+        post import_csv_path, params: { file: file_without_mandatory_field }
+        expect(flash[:error]).to be_truthy
       end
 
       it 'should redirect to root_path' do
