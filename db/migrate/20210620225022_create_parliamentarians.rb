55056101aba91657428eb14b69b425f35e9507cd
BrunaBelo <anabruna28@gmail.com>
2021-06-22 22:12:12 -0300

    Remove limit 2 to political_party

--- db/migrate/20210620225022_create_parliamentarians.rb
+++ db/migrate/20210620225022_create_parliamentarians.rb
@@ -4,7 +4,7 @@ class CreateParliamentarians < ActiveRecord::Migration[6.0]
       t.string :name, null: false
       t.string :number_person_id
       t.string :federation_unity, null: false, limit: 2
-      t.string :political_party, limit: 2
+      t.string :political_party
       t.string :card_parliamentary
       t.integer :register_id
       t.belongs_to :legislature, null: false, foreign_key: true
--- db/schema.rb
+++ db/schema.rb
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(version: 2021_06_21_023131) do
     t.string "name", null: false
     t.string "number_person_id"
     t.string "federation_unity", limit: 2, null: false
-    t.string "political_party", limit: 2
+    t.string "political_party"
     t.string "card_parliamentary"
     t.integer "register_id"
     t.bigint "legislature_id", null: false
