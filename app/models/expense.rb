6473be0af027acf3f9a88de3987b32c75c4f5ca0
BrunaBelo <anabruna28@gmail.com>
2021-06-23 11:28:58 -0300

    Add scope to sum total and biggest value of the net value

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -3,4 +3,8 @@ class Expense < ApplicationRecord
     belongs_to :provider
     belongs_to :quota_type
     belongs_to :expense_type
+
+    # scopes
+    scope :sum_total_net_value, -> { sum(:net_value) }
+    scope :biggest_value, -> { max_by(&:net_value) }
 end
